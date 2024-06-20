16c010e3e3b71674f8515c88d7c105e6fd09e4b1
rhymaado <mightydhiyamatra@yahoo.co.id>
2024-06-20 13:48:10 +0700

    state & props

--- src/pages/Function.tsx
+++ src/pages/Function.tsx
@@ -1,25 +1,56 @@
 // rfce
-function Profile(name: string, address: string): JSX.Element {
+import { useState } from "react";
+
+import publicFormIcon from "/form-icon.png";
+import assetsFormIcon from "../assets/form-icon.png";
+
+function Profile(props: { name: string; address: string }): JSX.Element {
   return (
-    <div>
-      <p>nama: {name}</p>
-      <p>domisili: {address}</p>
-    </div>
+    <>
+      <p>nama: {props.name}</p>
+      <p>domisili: {props.address}</p>
+    </>
   );
 }
 
+interface IUser {
+  id: number;
+  name: string;
+  address: string;
+}
+// type Status = "loading" | "success" | "failed";
+
 function Function() {
-  const user = {
-    id: 1,
-    name: "Budi",
-    address: "Jakarta",
-  };
+  // const [status, setStatus] = useState<Status>("loading");
+  // setStatus("")
+  const [user, setUser] = useState<IUser>();
+  // const user = {
+  //   id: 1,
+  //   name: "Budi",
+  //   address: "Jakarta",
+  // };
+  function getUser() {
+    setUser({
+      id: 1,
+      name: "Budi",
+      address: "Jakarta",
+    });
+  }
   return (
     <div>
-      <p>nama: {user.name}</p>
-      <p>domisili: {user.address}</p>
-      {Profile(user.name, user.address)}
-      {/* <Profile /> */}
+      <img src={publicFormIcon} alt="form-icon" />
+      <img src={assetsFormIcon} alt="form-icon" />
+      <button style={{ display: "block", width: "100%" }} onClick={getUser}>
+        Get User
+      </button>
+      {user && (
+        <>
+          <p>nama: {user.name}</p>
+          <p>domisili: {user.address}</p>
+          {/* {Profile(user)} */}
+          <Profile name={user.name} address={user.address} />
+        </>
+      )}
     </div>
   );
 }
