f41842aabca194d7fb64047a1f581a08a8ac9285
rhymaado <mightydhiyamatra@yahoo.co.id>
2024-06-20 11:35:51 +0700

    display data

--- src/pages/Function.tsx
+++ src/pages/Function.tsx
@@ -1,6 +1,27 @@
 // rfce
+function Profile(name: string, address: string): JSX.Element {
+  return (
+    <div>
+      <p>nama: {name}</p>
+      <p>domisili: {address}</p>
+    </div>
+  );
+}
+
 function Function() {
-  return <div>Function</div>;
+  const user = {
+    id: 1,
+    name: "Budi",
+    address: "Jakarta",
+  };
+  return (
+    <div>
+      <p>nama: {user.name}</p>
+      <p>domisili: {user.address}</p>
+      {Profile(user.name, user.address)}
+      {/* <Profile /> */}
+    </div>
+  );
 }
 
 export default Function;
